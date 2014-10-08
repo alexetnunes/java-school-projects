d3156293822a311e151aa8c7e4fbf72b163dd520
alexetnunes <alex.et.nunes@gmail.com>
2014-10-08 09:26:56 -0300

    Lab5 final

--- Labs/anunes-lab5/ex2/FileReadDemo.java
+++ Labs/anunes-lab5/ex2/FileReadDemo.java
@@ -1,3 +1,13 @@
+/*
+* FileReadDemo Class
+* Alex Nunes
+* 07/10/2014
+* Dalhousie University
+* Faculty of Computer Science
+*
+* Demonstrates a sorted insert function for
+* reading lines from a file
+*/
 import java.util.*;
 import java.io.*;
 public class FileReadDemo {
@@ -6,11 +16,11 @@ public class FileReadDemo {
 		ArrayList<String> list = new ArrayList<String>();
 		System.out.print("Enter the filename to read from: ");
 		String filename = kbd.nextLine();
-		
+
 		File file = new File(filename);
-		
+
 		Scanner inputFile = new Scanner(file);
-		
+
 		while (inputFile.hasNext())
 		{
 			String line = inputFile.nextLine();
@@ -19,7 +29,8 @@ public class FileReadDemo {
 		}
 		inputFile.close();
 	}
-	
+
+	//A function that inserts a string into an ArrayList in alphabetical order
 	public static void sortedInsert(ArrayList<String> list, String item){
 		if(list.isEmpty()){
 			list.add(item);
--- Labs/anunes-lab5/ex3/StringTokenizerDemo.java
+++ Labs/anunes-lab5/ex3/StringTokenizerDemo.java
@@ -1,18 +1,38 @@
+/*
+* StringTokenizerDemo Class
+* Alex Nunes
+* 07/10/2014
+* Dalhousie University
+* Faculty of Computer Science
+*
+* Demonstrates a sorted insert function for
+* reading words from a file and produces a
+* count of characters, words, and lines
+*/
 import java.util.*;
 import java.io.*;
 public class StringTokenizerDemo {
 	public static void main(String[] args) throws IOException {
-		
+
+		//Declare scanner
 		Scanner kbd = new Scanner(System.in);
+
+		//take in file
 		System.out.print("Enter the filename to read from: ");
-		ArrayList<String> words = new ArrayList<String>();
 		String filename = kbd.nextLine();
 		File file = new File(filename);
 		Scanner inputFile = new Scanner(file);
+
+		//Declare variables
+		ArrayList<String> words = new ArrayList<String>();
 		String line, word;
 		StringTokenizer token;
 		int lineCount = 0, characterCount=0;
-		
+
+		/* Loop through adding each word the the ArrayList
+		 * Add the word length to characterCount
+		 * Add one to the line count after each line
+		 */
 		while (inputFile.hasNext())
 		{
 			line = inputFile.nextLine();
@@ -26,7 +46,8 @@ public class StringTokenizerDemo {
 			lineCount++;
 		}
 		inputFile.close();
-		
+
+		//Print info
 		System.out.println("The number of characters (no spaces) in the file is: " + characterCount);
 		System.out.println("The number of words in the file is: " + words.size());
 		System.out.println("The number of lines in the file is: " + lineCount);
